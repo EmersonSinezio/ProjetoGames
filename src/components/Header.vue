60225e74efb0bff6095743cf2e7aebeb470e51b4
EmersonSinezio <emerson.sineziio@gmail.com>
2023-07-20 16:35:05 -0300

    #header_update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="header">
     <router-link to="/">
-      <h3>Games</h3>
+      <h3>Home</h3>
     </router-link>
     <ul class="header_links">
       <router-link to="/games">
-        <li class="link">Games</li>
+        <li class="link">Explore</li>
       </router-link>
       <router-link to="/about">
         <li class="link">About</li>
